95a5f7320f448fcabfbd38061865677080b45fd8
panprogramadorgh <barreroalvaro2007@gmail.com>
2025-01-13 20:44:34 +0100

    Eliminando archivos de gitignore

--- @types/core.d.ts
+++
@@ -1,7 +0,0 @@
-import { Endpoint } from "endpoint";
-export declare class QuickHTTP {
-    private binders;
-    get(path: string, handler: Endpoint.Handler): Endpoint.Binder<Endpoint.Handler, Endpoint.Methods.GET>;
-    post(path: string, handler: Endpoint.Handler): Endpoint.Binder<Endpoint.Handler, Endpoint.Methods.POST>;
-    listen(port: number, callback?: () => void): void;
-}
